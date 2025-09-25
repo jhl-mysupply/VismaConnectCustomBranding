8679201b0bf2034b570c16dee6cbbf499dafe0df
Jack Langelund <jack.langelund@visma.com>
2025-09-25 10:20:30 +0200

    Change to javascript file

--- VAXSmartCaseLogin.js
+++ VAXSmartCaseLogin.js
@@ -1,17 +1,31 @@
 
 
 
-
-console.log("VAXSmartCaseLogin.js loaded");
-
 document.addEventListener('DOMContentLoaded', function() {
     const header = document.querySelector('.client-name-header');
+    const backgroundImageElement = document.querySelector('#main-container');
+    const vismaLogoElement = document.querySelector('.visma-logo');
+    const applicationLogoElement = document.querySelector('.application-logo');
+    const loginBox = document.querySelector('.form-box');
+
     if(header) {
-        console.log("header located");
-        header.textContent = "Velkommen til RH medarbejdsportal";
+        header.textContent = "Velkommen til Region Hovedstadens medarbejdsportal";
     }
-    else {
-        console.log("Failed to locate header");
+
+    //if (backgroundImageElement) {
+    //    backgroundImageElement.style.backgroundImage = "url('datanet.png')";
+    //}
+
+    //if (vismaLogoElement) {
+    //    vismaLogoElement.src = "mysupply-name-logo.png";
+    //}
+
+    //if (applicationLogoElement) {
+    //    applicationLogoElement.src = "mysupply-logo.png";
+    //}
+
+    if (loginBox) {
+        loginBox.style.paddingTop = "20%";
     }
 
 });
