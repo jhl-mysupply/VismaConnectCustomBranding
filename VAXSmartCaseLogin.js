30b2488b6751b058632eabddd2e313a97d9407b7
Jack Langelund <jack.langelund@visma.com>
2025-09-25 08:41:02 +0200

    Updated the javascript file

--- VAXSmartCaseLogin.js
+++ VAXSmartCaseLogin.js
@@ -1,6 +1,8 @@
 
 const logo = document.querySelector('.application-logo');
 
+
+
 if(logo) {
     console.log("logo located");
     logo.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnCYiPwX1ylk6y66NDG7CgcmSMlsiNg4r-wQ&s";
@@ -8,3 +10,16 @@ if(logo) {
 else {
     console.log("Failed to locate logo");
 }
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const header = document.querySelector('.client-name-header');
+    if(header) {
+        console.log("header located");
+        header.textContent = "Velkommen til RH medarbejdsportal";
+    }
+    else {
+        console.log("Failed to locate header");
+    }
+
+});
