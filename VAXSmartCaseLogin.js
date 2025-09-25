60eae26e261e82e48aa163131a95bea4522292ae
Jack Langelund <jack.langelund@visma.com>
2025-09-25 09:26:08 +0200

    Change to js file

--- VAXSmartCaseLogin.js
+++ VAXSmartCaseLogin.js
@@ -1,16 +1,8 @@
 
-const logo = document.querySelector('.application-logo');
 
 
 
-if(logo) {
-    console.log("logo located");
-    logo.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnCYiPwX1ylk6y66NDG7CgcmSMlsiNg4r-wQ&s";
-}
-else {
-    console.log("Failed to locate logo");
-}
-
+console.log("VAXSmartCaseLogin.js loaded");
 
 document.addEventListener('DOMContentLoaded', function() {
     const header = document.querySelector('.client-name-header');
